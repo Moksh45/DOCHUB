e97df3c03aeb47e016e211478ce535552dea438b
manishjha-04 <stayconnectedwithmanish@gmail.com>
2023-04-09 03:18:52 +0530

    completed contracts

--- Contract/Verfication.sol
+++ Contract/Verfication.sol
@@ -62,5 +62,48 @@ contract Verification {
         --count_Exporters;
         }
         
-   
+    function alter_Exporter(address _add,string calldata    _newInfo) public
+    onlyOwner()
+     { 
+          assert(Exporters[_add].blockNumber!=0);
+             Exporters[_add].info=_newInfo; }
+
+    function changeOwner(address _newOwner) public 
+        onlyOwner  validAddress(_newOwner)   {  owner = _newOwner; }
+
+        event addHash(address indexed _exporter,string _ipfsHash);
+    function addDocHash (bytes32  hash,string calldata _ipfs) public 
+      canAddHash
+      {
+            assert(docHashes[hash].blockNumber==0 && docHashes[hash].minetime==0);
+            Record memory  newRecord = 
+            Record(block.number,block.timestamp,Exporters[msg.sender].info,_ipfs);
+            docHashes[hash] = newRecord; 
+            ++count_hashes;
+            emit addHash(msg.sender,_ipfs);
+      }
+      
+
+    function findDocHash (bytes32 _hash) 
+    external  view  returns (uint,uint,string memory,string memory) {
+       
+        return (docHashes[_hash].blockNumber,docHashes[_hash].minetime,docHashes[_hash].info,docHashes[_hash].ipfs_hash );
+        }
+
+    function deleteHash (bytes32 _hash) public
+    authorised_Exporter(_hash)
+    canAddHash
+    {
+    assert(docHashes[_hash].minetime!=0);
+    docHashes[_hash].blockNumber=0;
+    docHashes[_hash].minetime=0;
+    
+    --count_hashes;
+    
+    }
+    
+    function  getExporterInfo(address _add) external view returns(string memory){
+
+        return (Exporters[_add].info);
+    }
 }
